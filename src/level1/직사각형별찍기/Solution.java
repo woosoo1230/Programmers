728761f9e23e0c064b166c36b2bbc1769d71c71e
woosoo1230 <uusuu1230@gmail.com>
2021-12-08 01:56:09 +0900

    직사각형별찍기

---
+++ src/level1/직사각형별찍기/Solution.java
@@ -0,0 +1,20 @@
+package level1.직사각형별찍기;
+
+import java.util.Scanner;
+
+public class Solution {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		
+		//b만큼 반복하면서 a만큼 별찍기
+		for(int i = 0; i<b; i++) {
+			for(int j = 0; j<a; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+}
